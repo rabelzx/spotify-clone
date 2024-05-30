aab2a3f642c337bb356d87706a42336b7635e039
rabelzx <eric.rabelo21@gmail.com>
2024-05-30 16:29:11 -0300

    add mixs for you section

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight } from "lucide-react"; 
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from "lucide-react"; 
 import Image from "next/image";
 
 export default function Home() {
@@ -43,37 +43,77 @@ export default function Home() {
 
           <h1 className="mt-5 text-3xl font-semibold">Good morning</h1>
 
-          <div className="grid grid-cols-3 mt-5 gap-x-5 gap-y-3">
-            <a href="" className="bg-white/10 rounded ">
-             <Image src="/tame-impala.jpg" width={80} height={80} alt=""></Image>
-            </a>
-            <a href="" className="bg-white/10 rounded ">
-             <Image src="/tame-impala.jpg" width={80} height={80} alt=""></Image>
-            </a>
-            <a href="" className="bg-white/10 rounded ">
-             <Image src="/tame-impala.jpg" width={80} height={80} alt=""></Image>
-            </a>
-            <a href="" className="bg-white/10 rounded ">
-             <Image src="/tame-impala.jpg" width={80} height={80} alt=""></Image>
-            </a>
-            <a href="" className="bg-white/10 rounded ">
-             <Image src="/tame-impala.jpg" width={80} height={80} alt=""></Image>
-            </a>
-            <a href="" className="bg-white/10 rounded ">
-             <Image src="/tame-impala.jpg" width={80} height={80} alt=""></Image>
-            </a>
+          <div className="grid grid-cols-3 mt-5 gap-4">
+            <div className="bg-white/10 flex group rounded overflow:hidden items-center gap-4  hover:bg-white/20 transition-colors">
+             <Image src="/tame-impala.jpg" width={100} height={100} alt=""></Image>
+             <strong>Tame Impala</strong>
+             <button className="rounded-full bg-green-400 p-3 text-black ml-auto mr-8 invisible group-hover:visible"><Play/></button>
+            </div>
+            <div className="bg-white/10 flex group rounded overflow:hidden items-center gap-4  hover:bg-white/20 transition-colors">
+             <Image src="/tame-impala.jpg" width={100} height={100} alt=""></Image>
+             <strong>Tame Impala</strong>
+              <button className="rounded-full bg-green-400 p-3 text-black ml-auto mr-8 invisible group-hover:visible"><Play/></button>
+            </div>
+            <div className="bg-white/10 flex group rounded overflow:hidden items-center gap-4  hover:bg-white/20 transition-colors">
+             <Image src="/tame-impala.jpg" width={100} height={100} alt=""></Image>
+             <strong>Tame Impala</strong>
+              <button className="rounded-full bg-green-400 p-3 text-black ml-auto mr-8 invisible group-hover:visible"><Play/></button>
+            </div>
+            <div className="bg-white/10 flex group rounded overflow:hidden items-center gap-4  hover:bg-white/20 transition-colors">
+             <Image src="/tame-impala.jpg" width={100} height={100} alt=""></Image>
+             <strong>Tame Impala</strong>
+              <button className="rounded-full bg-green-400 p-3 text-black ml-auto mr-8 invisible group-hover:visible"><Play/></button>
+            </div>
+            <div className="bg-white/10 flex group rounded overflow:hidden items-center gap-4  hover:bg-white/20 transition-colors">
+             <Image src="/tame-impala.jpg" width={100} height={100} alt=""></Image>
+             <strong>Tame Impala</strong>
+              <button className="rounded-full bg-green-400 p-3 text-black ml-auto mr-8 invisible group-hover:visible"><Play/></button>
+            </div>
+            <div className="bg-white/10 flex group rounded overflow:hidden items-center gap-4 hover:bg-white/20 transition-colors">  
+             <Image src="/tame-impala.jpg" width={100} height={100} alt=""></Image>
+             <strong>Tame Impala</strong>
+              <button className="rounded-full bg-green-400 p-3 text-black ml-auto mr-8 invisible group-hover:visible"><Play/></button>
+            </div>
           </div>
 
           <h1 className="mt-10 text-2xl font-semibold">Mixs for you</h1>
 
-          <div className="grid grid-cols-4 mt-5 gap-x-5 gap-y-3">
-            <div className="bg-white/10 p-5">teste</div>
-            <div className="bg-white/10 p-5">teste</div>
-            <div className="bg-white/10 p-5">teste</div>
-            <div className="bg-white/10 p-5">teste</div>
-            <div className="bg-white/10 p-5">teste</div>
-            <div className="bg-white/10 p-5">teste</div>
-            <div className="bg-white/10 p-5">teste</div>
+          <div className="grid grid-cols-8 mt-4 gap-4">
+            <div className="bg-white/5 p-4 flex flex-col gap-2 rounded hover:bg-white/10">
+              <Image src="/tame-impala.jpg" className="w-full" width={120} height={120} alt=""></Image>
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Avenged Senfold, System of Down and More!  </span>
+            </div>
+            <div className="bg-white/5 p-4 flex flex-col gap-2 rounded hover:bg-white/10 transition-colors">
+              <Image src="/tame-impala.jpg" className="w-full" width={120} height={120} alt=""></Image>
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Avenged Senfold, System of Down and More!  </span>
+            </div>
+            <div className="bg-white/5 p-4 flex flex-col gap-2 rounded hover:bg-white/10 transition-colors">
+              <Image src="/tame-impala.jpg" className="w-full" width={120} height={120} alt=""></Image>
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Avenged Senfold, System of Down and More!  </span>
+            </div>
+            <div className="bg-white/5 p-4 flex flex-col gap-2 rounded hover:bg-white/10 transition-colors">
+              <Image src="/tame-impala.jpg" className="w-full" width={120} height={120} alt=""></Image>
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Avenged Senfold, System of Down and More!  </span>
+            </div>
+            <div className="bg-white/5 p-4 flex flex-col gap-2 rounded hover:bg-white/10 transition-colors">
+              <Image src="/tame-impala.jpg" className="w-full" width={120} height={120} alt=""></Image>
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Avenged Senfold, System of Down and More!  </span>
+            </div>
+            <div className="bg-white/5 p-4 flex flex-col gap-2 rounded hover:bg-white/10 transition-colors">
+              <Image src="/tame-impala.jpg" className="w-full" width={120} height={120} alt=""></Image>
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Avenged Senfold, System of Down and More!  </span>
+            </div>
+            <div className="bg-white/5 p-4 flex flex-col gap-2 rounded hover:bg-white/10 transition-colors">
+              <Image src="/tame-impala.jpg" className="w-full" width={120} height={120} alt=""></Image>
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Avenged Senfold, System of Down and More!  </span>
+            </div>
           </div>
         </main>
       </div>
