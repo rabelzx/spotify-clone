233da54589acd7a3a98d83706c4d7759a497e7c2
Eric Rabelo <eric.rabelo21@gmail.com>
2024-06-05 17:18:49 -0300

    add player section to footer

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,26 +1,33 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from "lucide-react"; 
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, SkipBack, SkipForward, Shuffle, Repeat } from "lucide-react"; 
 import Image from "next/image";
 
-function Playlist({ src }) {
+function Playlist({ src, title }) {
   return  <div className="bg-white/10 flex group rounded overflow:hidden items-center gap-4  hover:bg-white/20 transition-colors">
             <Image src={src} width={100} height={100} alt=""></Image>
-            <strong>Tame Impala</strong>
+            <strong>{title}</strong>
             <button className="rounded-full bg-green-400 p-3 text-black ml-auto mr-8 invisible group-hover:visible"><Play/></button>
           </div>
 }
 
-function Mix() {
+function Mix({ src, title }) {
   return <div className="bg-white/5 p-4 flex flex-col gap-2 rounded hover:bg-white/10">
-            <Image src="/tame-impala.jpg" className="w-full" width={120} height={120} alt=""></Image>
+            <Image src={src} className="w-full" width={120} height={120} alt=""></Image>
             <strong className="font-semibold">Daily Mix 1</strong>
-            <span className="text-sm text-zinc-400">Avenged Senfold, System of Down and More!  </span>
+            <span className="text-sm text-zinc-400">{title}</span>
           </div>
 }
 
 export default function Home() {
   const playlists = [
-    {src: "/tame-impala.jpg"}, 
-    {src: "/avenged-senfold.jpg"} 
+    {src: "/tame-impala.jpg", title: "Tame Impala"}, 
+    {src: "/avenged-senfold.jpg", title: "Avenged Sevenfold"},
+    {src: "/gorillaz.jpg", title: "Gorillaz"} 
+  ]
+
+  const mixes = [
+    {src: "/tame-impala.jpg", title: "Tame Impala"}, 
+    {src: "/avenged-senfold.jpg", title: "Avenged Sevenfold"},
+    {src: "/gorillaz.jpg", title: "Gorillaz"}
   ]
   
   return (
@@ -64,24 +71,29 @@ export default function Home() {
           <h1 className="mt-5 text-3xl font-semibold">Good morning</h1>
           <div className="grid grid-cols-3 mt-5 gap-4">
             {playlists.map((playlist, index) => (
-              <Playlist key={index} src={playlist.src}/>
+              <Playlist key={index} src={playlist.src} title={playlist.title}/>
             ))}
           </div>
           <h1 className="mt-10 text-2xl font-semibold">Mixs for you</h1>
           <div className="grid grid-cols-8 mt-4 gap-4">
-            <Mix/>
-            <Mix/>
-            <Mix/>
-            <Mix/>
-            <Mix/>
-            <Mix/>
-            <Mix/>
-            <Mix/>
+            {mixes.map((mix, index) => (
+              <Mix key={index} src={mix.src} title={mix.title}/>
+            ) )}
           </div>
         </main>
       </div>
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-        footer
+        <div className="flex justify-between">
+            <button>ESQUERDA</button>
+            <div className="flex space-x-5 items-center">
+              <Shuffle className="transition ease cursor-pointer hover:text-green-400"/>
+              <SkipBack className="transition ease cursor-pointer hover:text-green-400"/>
+              <button className="rounded-full p-3 text-black bg-white transition ease hover:bg-green-400"><Play/></button>
+              <SkipForward className="transition ease cursor-pointer hover:text-green-400"/>
+              <Repeat className="transition ease cursor-pointer hover:text-green-400"/>
+            </div>
+            <button>DIREITA</button>
+        </div>
       </footer>
     </div>
   );
